2b27fae26d9eabc82426912821338b2c6546fac8
ahmedmeshref <a.meshref@alustudent.com>
2020-06-02 19:20:44 +0200

    Add day_2 solutions

--- README.md
+++ README.md
@@ -3,12 +3,15 @@ Python and JavaScript solutions for June LeetCoding Challenge.
 
 **Note:** These solutions are provided **as a reference**.
 
-### Week1: Invert a Tree
+### day_1: Invert a Tree
 
 - [Inver Tree PY](/day_1/invert_tree.py)
 - [Inver Tree JS](/day_1/invert_tree.js) 
 
+### day_2: Delete Node in a Linked List
 
+- [Delete Node PY](/day_2/delete_node.py)
+- [Delete Node JS](/day_2/delete_node.js) 
 
 # Contributing
 
---
+++ day_2/delete_node.py
@@ -0,0 +1,34 @@
+"""
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Given linked list -- head = [4,5,1,9], which looks like following:
+
+Example 1:
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+Example 2:
+
+Note:
+The linked list will have at least two elements.
+All of the nodes' values will be unique.
+The given node will not be the tail and it will always be a valid node of the linked list.
+Do not return anything from your function.
+"""
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class Solution:
+    def delete_node(self, node):
+        """
+        delete_node function deletes the given node from the linked list
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val, node.next = node.next.val, node.next.next
+
