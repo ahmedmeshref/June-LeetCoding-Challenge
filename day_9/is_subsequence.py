4663b0794424716e59b3f10ed46a18960da8562b
ahmedmeshref <a.meshref@alustudent.com>
2020-06-10 02:09:23 +0200

    Add py solution for isSub challenge

--- day_9/is_subsequence.py
+++ day_9/is_subsequence.py
@@ -1,23 +1,65 @@
-def isSubsequence(s, t):
-    s, t = "!" + s, "!" + t
-    m, n = len(s), len(t)
-    dp = [[0] * m for _ in range(n)]
-    for i in range(n): dp[i][0] = 1
-
-    # for i, j in product(range(1, n), range(1, m)):
-    #     if s[j] == t[i]:
-    #         dp[i][j] = dp[i - 1][j - 1]
-    #     else:
-    #         dp[i][j] = dp[i - 1][j]
-
-    return dp[-1][-1]
+from collections import defaultdict
 
 
+# # solution for the follow up question
+# def isSubsequence(s, t):
+#     """
+#     isSubsequence function takes in two strings s and t, it returns true if s is subsequence of t, false otherwise
+#     :param s: string
+#     :param t: string
+#     :return: boolean
+#     """
+#     if len(s) > len(t): return False
+#     if len(s) == 0: return True
+#     d = defaultdict(list)
+#     for ind, char in enumerate(t):
+#         d[char].append(ind)
 #
-# s = ""
-# s = "!" + s
-# print(s)
+#     c_ind = -1
+#     for char in s:
+#         # check if it works without
+#         if char not in d:
+#             return False
+#         n_ind = binary_search(d[char], c_ind)
+#         if n_ind == -1:
+#             return False
+#         c_ind = n_ind
+#     return True
+#
+#
+# def binary_search(lst, target):
+#     """
+#     binary_binary_search finds and returns the index of the first element greater than target, -1 if such element
+#      doesn't exist.
+#     :param lst: Sorted [] of integers
+#     :param target: Integer
+#     :return: Integer
+#     """
+#     l = 0
+#     r = len(lst) - 1
+#     while l <= r:
+#         mid = (l + r) // 2
+#         if lst[mid] <= target:
+#             l = mid + 1
+#         else:
+#             if (mid == 0) or (lst[mid - 1] <= target):
+#                 return lst[mid]
+#             else:
+#                 r = mid - 1
+#     return -1
 
-s = "abc"
-t = "aknbzac"
-print(isSubsequence(s, t))
+# two pointers solution
+def isSubsequence(s, t):
+    i = j = 0
+    while i < len(s) and j < len(t):
+        if t[j] == s[i]:
+            i += 1
+        j += 1
+    return i == len(s)
+
+
+# the below lines are for testing only and it is not part of the solution
+if __name__ == "__main__":
+    sub = "abc"
+    string = "ahbgdc"
+    print(isSubsequence(sub, string))
