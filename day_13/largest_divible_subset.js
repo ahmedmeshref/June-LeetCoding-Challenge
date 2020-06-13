3a160e87b4118bfd43d036fd0a38903f57b6cd22
ahmedmeshref <a.meshref@alustudent.com>
2020-06-13 21:28:14 +0200

    Add js solution for day_13

--- day_13/largest_divible_subset.js
+++ day_13/largest_divible_subset.js
@@ -1,8 +1,36 @@
-
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
-let largestDivisibleSubset = function(nums) {
+let largestDivisibleSubset = function (nums) {
     if (nums.length < 2) return nums;
+    nums.sort((a, b) => a - b);
+    let res = [];
+    for (const num of nums) res.push([num]);
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] % nums[j] === 0 && res[i].length < res[j].length + 1) {
+                res[i] = res[j].concat([nums[i]]);
+            }
+        }
+    }
+    return max_ele(res);
 };
+
+let max_ele = (arr) => {
+    let max_len = 0,
+        max_so_far;
+    for (const lst of arr){
+        if (lst.length > max_len){
+            max_so_far = lst;
+            max_len = lst.length;
+        }
+    }
+    return max_so_far;
+}
+
+
+
+
+let n = [1, 4, 3, 10, 12];
+console.log(largestDivisibleSubset(n));
