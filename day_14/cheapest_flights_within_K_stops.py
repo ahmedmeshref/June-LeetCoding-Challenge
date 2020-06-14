aa8887ef715d2562896c6edc4fc739b73215e673
ahmedmeshref <a.meshref@alustudent.com>
2020-06-15 00:05:00 +0200

    add py solution for day_14 challenge

---
+++ day_14/cheapest_flights_within_K_stops.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+
+class Solution:
+    def findCheapestPrice(self, n, flights, src, dst, K):
+        graph = defaultdict(list)
+        for s, d, w in flights:
+            graph[s].append((d, w))
+        self.ans = float('inf')
+
+        def dfs(src, des, k, cost):
+            if src == des:
+                self.ans = min(self.ans, cost)
+                return
+            if k == 0:
+                return
+            for n_src, price in graph[src]:
+                new_cost = cost + price
+                if new_cost >= self.ans:
+                    continue
+                dfs(n_src, des, k - 1, new_cost)
+
+        dfs(src, dst, k + 1, 0)
+        return self.ans if self.ans != float('inf') else -1
+
+
+n = 4
+edges = [[0, 1, 100], [1, 3, 50], [3, 2, 100], [1, 2, 100], [0, 2, 500]]
+src = 0
+dst = 2
+k = 2
+g = Solution()
+print(g.findCheapestPrice(n, edges, src, dst, k))
